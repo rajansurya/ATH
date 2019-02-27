fffd80cad9aaac740dec65f743c59a65c764f3b1
rajan786 <rajan.gupta@skilrock.com>
2019-02-27 18:36:38 +0530

    sec

--- app/src/main/java/fieldx/mobile/com/atiyaherb/BaseActivity.kt
+++ app/src/main/java/fieldx/mobile/com/atiyaherb/BaseActivity.kt
@@ -3,6 +3,7 @@ package fieldx.mobile.com.atiyaherb
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.widget.TextView
 import dagger.android.AndroidInjection
 import javax.inject.Inject
@@ -21,8 +22,11 @@ abstract class BaseActivity : AppCompatActivity() {
         var textView = TextView(this)
         textView.text = text
         textView.tag = tag
-        textView.textSize = 13F
-        textView.setPadding(Globlefunction.dpToPx(8F, mContext).toInt(), Globlefunction.dpToPx(5F, mContext).toInt(), Globlefunction.dpToPx(8F, mContext).toInt(), Globlefunction.dpToPx(5F, mContext).toInt())
+        textView.textSize = 16F
+        textView.gravity=Gravity.CENTER
+        textView.setTextColor(resources.getColor(R.color.black,null))
+        textView.width=Globlefunction.dpToPx(100F, mContext).toInt()
+       // textView.setPadding(Globlefunction.dpToPx(8F, mContext).toInt(), Globlefunction.dpToPx(5F, mContext).toInt(), Globlefunction.dpToPx(8F, mContext).toInt(), Globlefunction.dpToPx(5F, mContext).toInt())
         textView.setBackgroundResource(R.drawable.label_bg)
         textView.setOnClickListener { println(textView.tag) }
         return textView
